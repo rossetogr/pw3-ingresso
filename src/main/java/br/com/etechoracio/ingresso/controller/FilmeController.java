23c484470c984c36bec8c78f72aa39182800ed5d
Dylan-Marinss <dylanmarins24@gmail.com>
2026-04-28 15:25:31 -0300

    controlls, repository e services

---
+++ src/main/java/br/com/etechoracio/ingresso/controller/FilmeController.java
@@ -0,0 +1,4 @@
+package br.com.etechoracio.ingresso.controller;
+
+public class FilmeController {
+}
---
+++ src/main/java/br/com/etechoracio/ingresso/repository/FilmeRepository.java
@@ -0,0 +1,4 @@
+package br.com.etechoracio.ingresso.repository;
+
+public interface FilmeRepository {
+}
